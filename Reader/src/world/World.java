2160fae745d7e4c6f9e53578cc093be215f4329a
54Cymru <54Cymru@540eae3c-6ad0-3524-a4c5-307e75023de4>
2011-11-15 11:52:59 +0000

    Añadida constructora del mundo a partir de un flujo de entrada

--- Reader/src/world/World.java
+++ Reader/src/world/World.java
@@ -1,8 +1,13 @@
 package world;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
+import world.ontobridge.OntoBridgeComponent;
 
 public class World implements Iterable<Component> {
 
@@ -24,6 +29,35 @@ public class World implements Iterable<Component> {
 		
 		for (Component component : wCopy.components) {
 			components.add(component.copy());
+		}
+	}
+	
+	/**
+	 * Construye un mundo mediante un flujo de entrada (fichero, entrada estándar, ...).
+	 * @param in Flujo de entrada.
+	 * */
+	public World(InputStream in) {
+		components = new ArrayList<Component>();
+		
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			
+			String line;
+			while ((line = br.readLine()) != null) {
+				if (line.startsWith("//"))
+					continue;
+				
+				String[] splittedLine = line.split(",");
+				Component c = new Component();
+				c.setWeight(Float.parseFloat(splittedLine[0].trim()));
+				c.setSource(new OntoBridgeComponent(splittedLine[1].trim(), OntoBridgeComponent.NAME));
+				c.setAction(new OntoBridgeComponent(splittedLine[2].trim(), OntoBridgeComponent.ACTION));
+				c.setTarget(new OntoBridgeComponent(splittedLine[3].trim(), OntoBridgeComponent.NAME));
+				
+				components.add(c);
+			}
+		} catch (Exception e) {
+			System.err.println("Error leyendo el estado del mundo: " + e.getMessage());
 		}
 	}
 	
