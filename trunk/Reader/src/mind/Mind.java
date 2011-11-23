0cbc089b3930a5696b94631a8025bebcf0485695
israelcr741 <israelcr741@540eae3c-6ad0-3524-a4c5-307e75023de4>
2011-11-23 18:07:19 +0000

    Rediseño de la mente del lector. Incluido HashMap < accion , Relaciones >.

--- trunk/Reader/src/mind/Mind.java
+++ trunk/Reader/src/mind/Mind.java
@@ -3,9 +3,11 @@ package mind;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
+import java.util.Map.Entry;
 
 /**
  * Implementación de la mente del lector.
@@ -13,26 +15,38 @@ import java.util.List;
  * @author Sergio Gutiérrez Mota e Israel Cabañas Ruiz
  *
  */
-public class Mind implements Iterable<Relation> {
+public class Mind {
 
 	/**
 	 * Lista de relaciones.
 	 * */
-	private List<Relation> relations;
+	private HashMap<String, HashSet<Relation>> relations;
 	
 	public Mind() {
-		relations = new ArrayList<Relation>();
+		relations = new HashMap<String, HashSet<Relation>>();
 	}
 
 	/**
 	 * Construye la mente mediante la copia de otra.
-	 * @param wCopy Mente referencia.
+	 * @param mCopy Mente referencia.
 	 * */
 	public Mind(Mind mCopy) {
-		relations = new ArrayList<Relation>();
 		
-		for (Relation relation : mCopy.relations) {
-			relations.add(relation.copy());
+		relations = new HashMap<String, HashSet<Relation>>();
+		
+		Iterator<Entry<String, HashSet<Relation>>> it = mCopy.relations.entrySet().iterator();
+		
+		while (it.hasNext()) {
+			
+			HashSet<Relation> auxRelations = new HashSet<Relation>();
+			
+			Entry<String, HashSet<Relation>> entryMCopy = it.next();
+						
+			for (Relation relation : entryMCopy.getValue()) {
+				auxRelations.add(relation.copy());
+			}
+			
+			relations.put(new String(entryMCopy.getKey()), auxRelations);
 		}
 	}
 	
@@ -41,7 +55,7 @@ public class Mind implements Iterable<Relation> {
 	 * @param in Flujo de entrada.
 	 * */
 	public Mind(InputStream in) {
-		relations = new ArrayList<Relation>();
+		relations = new HashMap<String, HashSet<Relation>>();
 		
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(in));
@@ -51,33 +65,65 @@ public class Mind implements Iterable<Relation> {
 				// Se pueden insertar comentarios iniciando la linea con "//"
 				if (line.startsWith("//"))
 					continue;
-				Relation c = new Relation(line);
-				relations.add(c);
+				Relation r = new Relation(line);
+				add(r);
 			}
 		} catch (Exception e) {
 			System.err.println("Error leyendo el estado de la mente: " + e.getMessage());
 		}
 	}
-
-	@Override
-	public Iterator<Relation> iterator() {
-		return relations.iterator();
+	
+	/**
+	 * Añade una relación a la mente.
+	 * @param r Relación a añadir.
+	 */
+	public void add(Relation r) {
+		
+		if (relations.containsKey(r.getAction().getName())) {
+			relations.get(r.getAction().getName()).add(r);
+		}
+		else {
+			HashSet<Relation> newRelations = new HashSet<Relation>();
+			newRelations.add(r);
+			relations.put(new String(r.getAction().getName()), newRelations);
+		}
 	}
 	
 	/**
-	 * @return El número de relaciones que hay en el mundo.
+	 * Elimina una relación de la mente.
+	 * @param r Relación a eliminar.
 	 */
-	public int getNumRelations() {
-		return relations.size();
+	public void remove(Relation r) {
+		
+		if (relations.containsKey(r.getAction().getName())) {
+			HashSet<Relation> setRelations = relations.get(r.getAction().getName());
+			setRelations.remove(r);
+			
+			if (setRelations.isEmpty()) {
+				relations.remove(r.getAction().getName());
+			}
+		}
 	}
 	
 	/**
-	 * Obtiene la i-ésima relación del listado de relaciones.
-	 * @param i La i-ésima relación dentro del listado de relaciones.
-	 * @return La relación número i.
+	 * Obtiene un HashMap con las acciones (pasadas por argumento) y las relaciones asociadas.
+	 * Otra forma de verlo es como un filtrado de la mente en el que obtenemos las relaciones
+	 * de determinadas acciones.
+	 * @param relationAction El conjunto de acciones cuyas relaciones van a tomarse.
+	 * @return HashMap con el par accion y sus relaciones, tras el filtrado.
 	 */
-	public Relation getRelation(int i) {
-		return relations.get(i);
+	public HashMap<String, Iterable<Relation>> getRelations(Collection<String> relationAction) {
+		
+		HashMap<String, Iterable<Relation>> retVal = new HashMap<String, Iterable<Relation>>();
+		
+		Iterator<String> itAction = relationAction.iterator();
+		
+		while (itAction.hasNext()) {
+			String action = itAction.next();
+			retVal.put(new String(action), relations.get(action));
+		}
+		
+		return retVal;		
 	}
 	
 	@Override
@@ -91,17 +137,34 @@ public class Mind implements Iterable<Relation> {
 	 * */
 	public String toShortString() {
 		String shortStr = "";
-		for (Relation r : relations) {
-			shortStr += r.toShortString();
+		
+		Iterator<Entry<String, HashSet<Relation>>> it = relations.entrySet().iterator();
+		
+		while (it.hasNext()) {
+		
+			Entry<String, HashSet<Relation>> entry = it.next();
+						
+			for (Relation relation : entry.getValue()) {
+				shortStr += relation.toShortString();
+			}
 		}
+		
 		return shortStr;
 	}
 	
 	public String toStringSegment() {
 		String retVal = "Segmento\n";
-		for (int i = 0; i < relations.size(); i++) {
-			retVal += relations.get(i).toStringRelation() + "\n";
+		Iterator<Entry<String, HashSet<Relation>>> it = relations.entrySet().iterator();
+		
+		while (it.hasNext()) {
+		
+			Entry<String, HashSet<Relation>> entry = it.next();
+						
+			for (Relation relation : entry.getValue()) {
+				retVal += relation.toShortString();
+			}
 		}
+
 		return retVal;
 	}
 }
