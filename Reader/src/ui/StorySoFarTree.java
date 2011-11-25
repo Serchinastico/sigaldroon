f8224f9aa8d21a642d1674e20df6c94237e857c7
israelcr741 <israelcr741@540eae3c-6ad0-3524-a4c5-307e75023de4>
2011-11-25 19:38:01 +0000

    Separados componentes del JFrame: StorySoFarJTree.java, MainMenu.java Aplicado patrón Mediator en el JFrame. Aplicado parte del patrón Observer en Reader y JFrame. Arreglados los fallos provocados por el nuevo diseño de la mente. Interfaz utilizable como antes.

---
+++ Reader/src/ui/StorySoFarTree.java
@@ -0,0 +1,121 @@
+package ui;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.JTree;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+
+import mind.Mind;
+import mind.Relation;
+
+/**
+ * Componente para representar en jerarquía de árbol una historia.
+ * 
+ * @author Sergio Gutiérrez Mota e Israel Cabañas Ruiz
+ *
+ */
+public class StorySoFarTree extends JTree {
+
+	/**
+	 * Default serial ID.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Frame contenedor de este menú.
+	 */
+	private StoryJFrame frame;
+
+	/**
+	 * Estructura a cargar en el árbol.
+	 */
+	private DefaultTreeModel model;
+
+	/**
+	 * Título del árbol.
+	 */
+	private DefaultMutableTreeNode titleTree;
+
+	/**
+	 * Constructora con el panel en el que se va a colocar el componente.
+	 */
+	public StorySoFarTree(StoryJFrame f) {
+		super();
+		frame = f;
+		titleTree = new DefaultMutableTreeNode("Mito");
+		model = new DefaultTreeModel(titleTree);
+		this.setModel(model);
+		this.addTreeSelectionListener(new TreeSelectionListener() {
+			public void valueChanged(TreeSelectionEvent e) {
+				selectionPerformed();
+			}
+		});
+		frame.getjScrollPaneLeft().setViewportView(this);
+	}
+
+	/**
+	 * Listener para la selección de elementos del árbol.
+	 */
+	private void selectionPerformed() {
+		DefaultMutableTreeNode node = (DefaultMutableTreeNode)
+		this.getLastSelectedPathComponent();
+
+		if (node == null) return;
+
+		int depthLevel = node.getLevel();
+
+		switch (depthLevel) {
+		case 1:
+			// Es un segmento
+			StoryMutableTreeNode segment = (StoryMutableTreeNode) node;
+			String segmentContent = frame.getObservableReader().getStorySoFar().get(segment.getSegmentPosition()).toStringSegment();
+			frame.getTextAreaVisualizacion().setText(segmentContent);
+			break;
+		case 2:
+			// Es una relación
+			StoryMutableTreeNode relation = (StoryMutableTreeNode) node;
+			Iterator<Relation> itRelation = frame.getObservableReader().getStorySoFar().get(relation.getSegmentPosition()).iterator();
+			int j = 0;
+			while (j < relation.getRelationPosition()) {
+				itRelation.next();
+				j++;
+			}
+			String relationContent = itRelation.next().toStringRelation();
+			frame.getTextAreaVisualizacion().setText(relationContent);
+			break;
+		}
+
+	}
+
+	/**
+	 * Inserta una historia en el árbol.
+	 * @param story Historia a insertar.
+	 */
+	public void insertStory(ArrayList<Mind> story) {
+		for (int i = 0; i < story.size(); i++) {
+			StoryMutableTreeNode segment = new StoryMutableTreeNode("Segmento "+i,i);
+			model.insertNodeInto(segment, titleTree, i);
+
+			int j = 0;
+			Iterator<Relation> itRelation = story.get(i).iterator();
+			while (itRelation.hasNext()) {
+				itRelation.next();
+				StoryMutableTreeNode relation = new StoryMutableTreeNode("Relación "+j,i,j);
+				model.insertNodeInto(relation, segment, j);
+				j++;
+			}
+
+		}
+	}
+
+	/**
+	 * Recarga el árbol en la interfaz.
+	 */
+	public void reload() {
+		model.reload();
+	}
+}
