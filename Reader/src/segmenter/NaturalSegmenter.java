6dd4ae993f526ba159e0d934acf98bb90f0d5f16
israelcr741 <israelcr741@540eae3c-6ad0-3524-a4c5-307e75023de4>
2011-12-05 00:27:21 +0000

    Avance con encajes básicos para el segmentador.

--- Reader/src/segmenter/NaturalSegmenter.java
+++ Reader/src/segmenter/NaturalSegmenter.java
@@ -1,6 +1,8 @@
 package segmenter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 import es.ucm.fdi.gaia.ontobridge.OntoBridge;
 
@@ -22,36 +24,9 @@ public class NaturalSegmenter implements ISegmenter {
 
 	@Override
 	public String generateSegment(ChangedMind m) {
-		OntoBridge ob = OntoBridgeSingleton.getInstance();
 		String segmentText = "";
-		ArrayList<String> singleSentences = new ArrayList<String>();
-		ArrayList<Change> filteredChanges = filterChanges(m.getChanges());
-		for (Change c : filteredChanges) {
-			Relation resultRelation = c.getAfter();
-			if (!ob.existsClass(resultRelation.getAction())) {
-				// Obtener las propiedades
-				ArrayList<String> properties = new ArrayList<String>();
-				ArrayList<String> values = new ArrayList<String>();
-				ob.listInstancePropertiesValues(resultRelation.getAction(), properties, values);
-				// Obtener el valor del verbo en pasado
-				String past = "";
-				String prepositionDirectObject = "";
-				for (int j = 0; j < properties.size(); j++) {
-					if (properties.get(j).contains("PasadoSingular"))
-						past = values.get(j).split("\\^\\^")[0];
-					else if (properties.get(j).contains("PreposicionObjetoDirecto"))
-						prepositionDirectObject = values.get(j).split("\\^\\^")[0];
-				}
-				// Formar la frase y añadirla a los String del grupo de frases solas
-				String sentence = resultRelation.getSource() + " " + past;
-				if (resultRelation.getTarget() != null) 
-					sentence += " " + prepositionDirectObject + " " + resultRelation.getTarget();
-				singleSentences.add(sentence);
-			}
-		}
-		for (String sentence : singleSentences) {
-			segmentText += sentence + ". ";
-		}
+		ArrayList<Relation> filteredRelations = filterChanges(m.getChanges());
+		segmentText = makeText(filteredRelations);
 		return segmentText;
 	}
 	
@@ -62,8 +37,8 @@ public class NaturalSegmenter implements ISegmenter {
 	 * @param changes Cambios realizados.
 	 * @return Cambios filtrados.
 	 */
-	private ArrayList<Change> filterChanges(ArrayList<Change> changes) {
-		ArrayList<Change> filteredChanges = new ArrayList<Change>();
+	private ArrayList<Relation> filterChanges(ArrayList<Change> changes) {
+		ArrayList<Relation> filteredChanges = new ArrayList<Relation>();
 		for (int i = 0; i < changes.size(); i++) {
 			// Solo es necesario mirar en los cambios que hay adelante
 			// y ver si la relación resultante es usada en otro cambio de forma inicial
@@ -73,22 +48,104 @@ public class NaturalSegmenter implements ISegmenter {
 				changeToInclude = !changes.get(i).getAfter().equals(changes.get(j).getBefore());
 			}
 			if (changeToInclude) 
-				filteredChanges.add(changes.get(i));
+				filteredChanges.add(changes.get(i).getAfter());
 		}
 		return filteredChanges;
 	}
 
 	@Override
 	public String generateInitialSegment(Mind m) {
-		OntoBridge ob = OntoBridgeSingleton.getInstance();
 		String segmentText = "";
-		ArrayList<String> singleSentences = new ArrayList<String>();
-		for (Relation resultRelation : m) {
-			if (!ob.existsClass(resultRelation.getAction())) {
+		ArrayList<Relation> mindRelations = new ArrayList<Relation>();
+		for (Relation relation : m)
+			mindRelations.add(relation);
+		segmentText += makeText(mindRelations);
+		return segmentText;
+	}
+
+	/**
+	 * Genera todos los párrafos para este segmento.
+	 * @param textRelations Las relaciones a tener en cuenta para generar el texto.
+	 * @return El string con el texto.
+	 */
+	private String makeText(ArrayList<Relation> textRelations) {
+		String text = "";
+		
+		// Categorizamos las relaciones por Place
+		HashMap<String, ArrayList<Relation>> relationsByPlace = new HashMap<String, ArrayList<Relation>>();
+		for (Relation relation : textRelations) {
+			String place = "";
+			if (relation.getPlace() == null) {
+				place = "NoPlace";
+			}
+			if (relationsByPlace.containsKey(place)) {
+				relationsByPlace.get(place).add(relation);
+			}
+			else {
+				ArrayList<Relation> newPlaceRelation = new ArrayList<Relation>();
+				newPlaceRelation.add(relation);
+				relationsByPlace.put(place, newPlaceRelation);
+			}
+		}
+		
+		// Para cada categoría de Place, se crea un un párrafo
+		for (Entry<String, ArrayList<Relation>> entry : relationsByPlace.entrySet()) {
+			text += makeParagrah(entry.getValue());
+		}
+		
+		return text;
+	}
+	
+	/**
+	 * Genera el texto de un párrafo. Cada párrafo está relacionado con sucesos de un lugar,
+	 * excepto los que no tienen lugar que están juntos en uno.
+	 * @param paragraphRelations Relaciones que van a formar parte del párrafo.
+	 * @return El string con el texto.
+	 */
+	private String makeParagrah(ArrayList<Relation> paragraphRelations) {
+		String paragraph = "";
+		
+		// Categorizamos las relaciones por Source
+		HashMap<String, ArrayList<Relation>> relationsBySource = new HashMap<String, ArrayList<Relation>>();
+		for (Relation relation : paragraphRelations) {
+			if (relationsBySource.containsKey(relation.getSource())) {
+				relationsBySource.get(relation.getSource()).add(relation);
+			}
+			else {
+				ArrayList<Relation> newSourceRelation = new ArrayList<Relation>();
+				newSourceRelation.add(relation);
+				relationsBySource.put(relation.getSource(), newSourceRelation);
+			}
+		}
+		
+		// Para cada categoría de Source, se crea una frase
+		for (Entry<String, ArrayList<Relation>> entry : relationsBySource.entrySet()) {
+			paragraph += makePhrase(entry.getValue());
+		}
+		
+		paragraph += "\n";
+		return paragraph;
+	}
+	
+	/**
+	 * Genera el texto relacionado con un personaje y lugar si existe.
+	 * El lugar y el personaje es el mismo para todas las relaciones.
+	 * @param phraseRelations Relaciones que van a generar la frase.
+	 * @return El string con el texto.
+	 */
+	private String makePhrase(ArrayList<Relation> phraseRelations) {
+		OntoBridge ob = OntoBridgeSingleton.getInstance();
+		String phrase = "";
+		if (phraseRelations.get(0).getPlace() != null) 
+			phrase += "En " + phraseRelations.get(0).getPlace() + ", ";
+		phrase += phraseRelations.get(0).getSource();
+		for (Relation relation : phraseRelations) {
+			// Por ahora encaje básico, la gracia es tener un listado de encajes posibles
+			if (!ob.existsClass(relation.getAction())) {
 				// Obtener las propiedades
 				ArrayList<String> properties = new ArrayList<String>();
 				ArrayList<String> values = new ArrayList<String>();
-				ob.listInstancePropertiesValues(resultRelation.getAction(), properties, values);
+				ob.listInstancePropertiesValues(relation.getAction(), properties, values);
 				// Obtener el valor del verbo en pasado
 				String past = "";
 				String prepositionDirectObject = "";
@@ -99,16 +156,12 @@ public class NaturalSegmenter implements ISegmenter {
 						prepositionDirectObject = values.get(j).split("\\^\\^")[0];
 				}
 				// Formar la frase y añadirla a los String del grupo de frases solas
-				String sentence = resultRelation.getSource() + " " + past;
-				if (resultRelation.getTarget() != null) 
-					sentence += " " + prepositionDirectObject + " " + resultRelation.getTarget();
-				singleSentences.add(sentence);
+				phrase += " " + past;
+				if (relation.getTarget() != null) 
+					phrase += " " + prepositionDirectObject + " " + relation.getTarget();
 			}
 		}
-		for (String sentence : singleSentences) {
-			segmentText += sentence + ". ";
-		}
-		return segmentText;
+		phrase += ". ";
+		return phrase;
 	}
-
 }
